e0d64c1fc5e2d2bb3b412c5f0125f306cb44942c
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-14 15:18:42 +0530

    Create ai_model.py

---
+++ backend/ai_model.py
@@ -0,0 +1,13 @@
+# AgriVision AI Model Handler
+
+def predict_disease(image):
+
+    # Later we will connect trained AI model here
+
+    result = {
+        "disease": "Leaf Blight",
+        "confidence": "92%",
+        "treatment": "Use recommended crop protection methods."
+    }
+
+    return result
