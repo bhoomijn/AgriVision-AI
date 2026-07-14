8e72bf9a3ed0e6e50ac21cd10e9ad2b077cec5ac
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-14 17:37:53 +0530

    Update ai_model.py

--- backend/ai_model.py
+++ backend/ai_model.py
@@ -1,13 +1,51 @@
-# AgriVision AI Model Handler
+import os
+import requests
+import io
+
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+
+API_URL = "https://api-inference.huggingface.co/models/malifiahm/plant_disease_classification"
+
 
 def predict_disease(image):
 
-    # Later we will connect trained AI model here
+    try:
+        buffer = io.BytesIO()
+        image.save(buffer, format="JPEG")
 
-    result = {
-        "disease": "Leaf Blight",
-        "confidence": "92%",
-        "treatment": "Use recommended crop protection methods."
-    }
+        headers = {
+            "Authorization": f"Bearer {HF_TOKEN}"
+        }
 
-    return result
+        response = requests.post(
+            API_URL,
+            headers=headers,
+            data=buffer.getvalue()
+        )
+
+        result = response.json()
+
+        if isinstance(result, list):
+            prediction = result[0]
+
+            return {
+                "disease": prediction["label"],
+                "confidence": str(round(prediction["score"] * 100, 2)) + "%",
+                "treatment": "Use recommended crop protection methods."
+            }
+
+        return {
+            "disease": "Model processing...",
+            "confidence": "0%",
+            "treatment": "Try again."
+        }
+
+    except Exception as e:
+
+        return {
+            "disease": "AI Error",
+            "confidence": "0%",
+            "treatment": str(e)
+        }
